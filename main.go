65dd01280e485c7f54e6880eb1fd7ff345ee656b
KateGritsay <fk0306@mail.ru>
2019-06-16 01:49:57 +0300

    refactored Ex2

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"strconv"
+	"strings"
 	"unicode"
 )
 
@@ -17,39 +18,32 @@ func Exercise2(startString string) string{
 	var result string
 
 	for  i := 0; i < len(runeString); i++ {
-		if unicode.IsLetter(runeString[i]) {
-		//	fmt.Printf("%v", string(runeString[i]))
-			result += string(runeString[i])
+		if !unicode.IsLetter(runeString[i]) {
+			continue
+		}
+		result += string(runeString[i])
 
-			if i == len(runeString)-1 {
-				break
-			}
-			if unicode.IsLetter(runeString[i+1]){
-				continue
-			}
-
-			num:= string(runeString[i+1])
-			for j := i + 2; j < len(runeString); j++ {
-				if unicode.IsLetter(runeString[j]) {
-					break
-				} else {
-					num += string(runeString[j])
-				}
-			}
-			h, err := strconv.ParseInt(num, 0, 16)
-
-			if err != nil {
-				fmt.Printf("Error parse string to int")
-			}
-			for x := 0; x < int(h)-1; x++ {
-				//fmt.Printf("%v", string(runeString[i]))
-				result += string(runeString[i])
-			}
-
-		} else {
+		if i == len(runeString)-1 {
+			break
+		}
+		if unicode.IsLetter(runeString[i+1]){
 			continue
 		}
 
+		num:= string(runeString[i+1])
+		for j := i + 2; j < len(runeString); j++ {
+			if unicode.IsLetter(runeString[j]) {
+				break
+			} else {
+				num += string(runeString[j])
+			}
+		}
+		h, err := strconv.ParseInt(num, 0, 16)
+
+		if err != nil {
+			fmt.Printf("Error parse string to int")
+		}
+		result += strings.Repeat(string(runeString[i]), int(h)-1)
 	}
 	return result;
 }
